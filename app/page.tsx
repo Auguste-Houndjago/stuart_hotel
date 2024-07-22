cf7c7bb8b71460cd85b055ffaf4505832696cba9
Auguste-Houndjago <augustehoundjago@gmail.com>
2024-07-22 08:32:56 +0000

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-// import Image from "next/image";
-import { PageProps } from '../.next/types/app/layout';
- import AddHotelForm from "@/components/hotel/AddHotelForm";
+import { HotelWithRooms } from "@/components/hotel/AddHotelForm"
+import HotelCard from "@/components/hotel/HotelCard"
 
 
-// import { UserButton } from "@clerk/nextjs";
-
-
-
-
-export default function Home() {
+function HotelList({hotels}: {hotels: HotelWithRooms[]}) {
   return (
-    <div>
-      home page
-      hello komy
-
-
+    <div className="grid grid-cols-1 sm:grid-cols-2=1 lg:grid-cols-1 gap-x-8 gap-y-12 mt-4">
+      {/* {hotels.map((hotel) => <div key={hotel.id}>
+        <HotelCard hotel={hotel} />
+      </div>)} */}
+      <h1 className="m-5">
+        Bienvenue sur la page: le projet est toujours en cours
+      </h1>
+      <div className="m-1">
+        <p>connecter vous et ajouter votre hotel en suivant le formulaire </p>
+      </div>
     </div>
-  );
+  )
 }
+
+export default HotelList
