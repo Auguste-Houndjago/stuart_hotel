11cff24657fc4201c1fbb8f41d62570b7794beb5
Auguste-Houndjago <augustehoundjago@gmail.com>
2024-08-10 19:43:37 +0000

    up

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -26,7 +26,7 @@ export default function NavBar() {
             <div className="flex justify-between ">
             <div className='flex items-center gap-1  cursor-pointer' onClick={()=> router.push('/')}>
           <Image src='/logo.svg' alt="logo" width='30' height='30' />
-          <div className="font-bold hidden md:block text-xl " >Togo_hotel</div>
+          <div className="font-bold hidden md:block text-xl " >Stuart_hotel</div>
         </div>
         <SearchInput />
             <div className="flex gap-3 items-center" >
